2c7f0db740cad6cea111a34d902864a29bd36fe7
Claudiaam18 <158302256+Claudiaam18@users.noreply.github.com>
2025-05-23 08:17:31 -0600

    Ejercicios

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -4,42 +4,33 @@ import java.util.*;
 
 public class E01MeetingRooms {
 
-    public int minMeetingRooms(ArrayList<ed.lab.MeetingInterval> meetingIntervals) {
-        return 0;
-    }
-
-    public class MeetingInterval {
-        public int start;
-        public int end;
-
-        public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-            if (meetingIntervals == null || meetingIntervals.size() == 0) {
-                return 0;
-            }
-
-            List<Integer> inicios = new ArrayList<>();
-            List<Integer> finales = new ArrayList<>();
-
-            for (MeetingInterval m : meetingIntervals) {
-                inicios.add(m.start);
-                finales.add(m.end);
-            }
-
-            Collections.sort(inicios);
-            Collections.sort(finales);
-
-            int salas = 0;
-            int fin = 0;
-
-            for (int i = 0; i < inicios.size(); i++) {
-                if (inicios.get(i) < finales.get(fin)) {
-                    salas++;
-                } else {
-                    fin++;
-                }
-            }
-
-            return salas;
+    public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
+        if (meetingIntervals == null || meetingIntervals.isEmpty()) {
+            return 0;
         }
+
+        List<Integer> inicios = new ArrayList<>();
+        List<Integer> finales = new ArrayList<>();
+
+        for (MeetingInterval m : meetingIntervals) {
+            inicios.add(m.startTime());
+            finales.add(m.endTime());
+        }
+
+        Collections.sort(inicios);
+        Collections.sort(finales);
+
+        int salas = 0;
+        int fin = 0;
+
+        for (int i = 0; i < inicios.size(); i++) {
+            if (inicios.get(i) < finales.get(fin)) {
+                salas++;
+            } else {
+                fin++;
+            }
+        }
+
+        return salas;
     }
 }
