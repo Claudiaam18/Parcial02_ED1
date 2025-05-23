81eeae48a63124c46526c735e1a6f3753469f926
Claudiaam18 <158302256+Claudiaam18@users.noreply.github.com>
2025-05-23 08:02:06 -0600

    Ejercicios

--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -3,11 +3,17 @@ package ed.lab;
 import java.util.HashMap;
 
 public class E02Logger {
+    private HashMap<String, Integer> mensajes;
 
     public E02Logger() {
+        mensajes = new HashMap<>();
     }
 
     public boolean shouldPrintMessage(int timestamp, String message) {
+        if (!mensajes.containsKey(message)) {
+            mensajes.put(message, timestamp + 10);
+            return true;
+        }
         return false;
     }
 }
